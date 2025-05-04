06eae30e68c3378ed188cd585fef971864426bb8
JesusVicken <jesus.viccken@gmail.com>
2025-05-03 21:47:21 -0300

    feat: ajuste de Hero

--- cppextremebsb/src/app/_components/hero.tsx
+++ cppextremebsb/src/app/_components/hero.tsx
@@ -1,9 +1,13 @@
 import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
+import ImageCpp from '../../../public/cpp.jpeg'
+import Image from "next/image"
 export function Hero() {
     return (
         <section className="bg-[#060505] text-white relative overflow-hidden">
-            <div>
-                <article className='grid grid-cols-1 lg:grid-cols-2'>
+            <div className='container mx-auto pt-16 pb-16 md:pb-16 px-4 relative'>
+
+                <article className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+
                     <div className='space-y-6'>
                         <h1 className="text-3xl md:text-4xl font-bold leading-10">
                             CPP Extreme BSB: Mais que Remo, é Estilo de Vida.
@@ -25,10 +29,18 @@ export function Hero() {
                         </div>
                     </div>
 
-                    <div>
-                       <h1>foto da CPP</h1> 
+                    <div className='hidden md:block h-full relative'>
+                        <Image
+                            src={ImageCpp}
+                            alt='logo CPP'
+                            className='object-contain'
+                            fill
+                            sizes='(max-width: 760px) 0px, 50vw'
+                            quality={100}
+                            priority
+                        />
                     </div>
-                
+
                 </article>
             </div>
 
