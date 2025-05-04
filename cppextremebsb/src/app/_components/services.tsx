b127e54c49749b00322bd53c5bff6ca48ac7ff0b
JesusVicken <jesus.viccken@gmail.com>
2025-05-04 10:17:34 -0300

    feat: ajuste de responsividade services

--- cppextremebsb/src/app/_components/services.tsx
+++ cppextremebsb/src/app/_components/services.tsx
@@ -111,7 +111,7 @@ export function Services() {
     }
 
     return (
-        <section className="bg-white py-16">
+        <section className="bg-white py-8 md:py-16">
             <div className="container mx-auto px-4">
                 <h2 className="text-4xl font-bold text-black mb-12 text-center">NOSSOS SERVIÇOS</h2>
 
